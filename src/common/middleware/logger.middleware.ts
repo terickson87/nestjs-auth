2ac76d9b3cfc092e1a41a7415aafdd98b9789db9
Todd Ericskon <todd.erickson87@gmail.com>
2024-05-19 08:49:50 -0700

    Adding logging, figured out that I was using the wrong token. I needed to use the access token and not the refresh token.

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -3,6 +3,8 @@ import { NextFunction, Request, Response } from 'express';
 export function logger(req: Request, _res: Response, next: NextFunction) {
   console.log('*****Start Request*****');
   console.log(`Request arrived: ${new Date(Date.now()).toISOString()}`);
+  console.log(`Request Location:\n ${req.path}`);
+  console.log(`Request Type:\n ${req.method}`);
   console.log(`Request Body:\n ${JSON.stringify(req.body)}`);
   console.log(`Request Headers:\n ${JSON.stringify(req.headers)}`);
   console.log('*****End Request.*****\n');
